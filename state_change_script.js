5f9a5397e9ffecf97860f7121493f666c60cd479
Mohammad Refaat <mrefaat@google.com>
2017-05-30 14:58:51 -0700

    reformat

--- state_change_script.js
+++ state_change_script.js
@@ -1,34 +1,39 @@
 function pushStateCall() {
-console.log("calling pushState");
-var stateObj = { foo: "dummy" };
-history.pushState(stateObj, "new page", "empty_landing_page.html");
+  console.log("calling pushState");
+  var stateObj = {foo: "dummy"};
+  history.pushState(stateObj, "new page", "empty_landing_page.html");
 }
-function HashChange() {
-}
-HashChange.counter = 0;
-HashChange.originalHref = window.location.href;
-HashChange.usingLocationHash = function () {
-  window.location.hash = "changed" + HashChange.counter;
-   HashChange.counter ++;
-} 
-HashChange.usingLocationHref = function () {
-	window.location.href = HashChange.originalHref + '#changed' + HashChange.counter;
-	HashChange.counter ++;
-}
+
+var hashChange = {"counter" = 0, "originalHref" = window.location.href}
+
+hashChange.usingLocationHash = function () {
+  console.log("Changing the location.hash");
+  window.location.hash = "changed" + HashChange.counter ++;
+};
+
+hashChange.usingLocationHref = function () {
+  console.log("Changing the location.href to override hash");
+  window.location.href = this.originalHref +
+    '#changed' + this.counter ++;
+};
+
 function replaceStateCall() {
-	console.log("replacing state");
-	var stateObj = {foo:"dummy"};
-	history.replaceState(stateObj, "new page", "empty_landing_page.html");
+  console.log("replacing state");
+  var stateObj = {foo: "dummy"};
+  history.replaceState(stateObj, "new page", "empty_landing_page.html");
 }
+
 function locationHrefChange() {
-	console.log("chaning href location");
-	window.location.href = "empty_landing_page.html";
+  console.log("chaning href location");
+  window.location.href = "empty_landing_page.html";
 }
+
 function locationReplaceCall() {
-	console.log("replace location");
-	window.location.replace("empty_landing_page.html");
+  console.log("replace location");
+  window.location.replace("empty_landing_page.html");
 }
+
 function locationAssignCall() {
-	console.log("assign location");
-	window.location.assign("empty_landing_page.html");
+  console.log("assign location");
+  window.location.assign("empty_landing_page.html");
 }
